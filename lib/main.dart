3c51d4c943a2f00ca7e147c7474f77e8e5283df9
Iftikhar Ahmed <ifti3061@gmail.com>
2025-01-19 14:55:58 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api
 
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('My Mood '),
+          title: Text('My Mood'),
           backgroundColor: Colors.black26,
         ),
         body: MyMoodPage(),
@@ -24,33 +25,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyMoodPage extends StatelessWidget {
-  const MyMoodPage({super.key});
+class MyMoodPage extends StatefulWidget {
+  const MyMoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyMoodPage> createState() => _MyMoodPageState();
+}
+
+class _MyMoodPageState extends State<MyMoodPage> {
+  var myMood = ['angry', 'happy', 'laughing', 'romantic', 'sad'];
+  var myCurrentMood = 'angry';
+
+  void predictMyMood() {
+    final _random = new Random();
+
+    var element = myMood[_random.nextInt(myMood.length)];
+
+    setState(() {
+      myCurrentMood = element;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-            flex:2,
-    child: Image(
-      image: AssetImage("images/happy.png"),
-        )
-      ),
-    Expanded(
-      flex: 1,
-        child: Image(
-          image: AssetImage("images/happy.png"),
-      ),
-
-    ),
-    Expanded(
-      flex: 2,
-    child: Image(
-    image: AssetImage("images/happy.png"),
-    )
-    )
-    ],
+        Row(children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Image.asset("images/$myCurrentMood.png"),
+            ),
+          ),
+        ]),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black26,
+            ),
+            child: Text('Predict My Mood'),
+            onPressed: () {
+              predictMyMood();
+            },
+          ),
+        ),
+      ],
     );
   }
 }
